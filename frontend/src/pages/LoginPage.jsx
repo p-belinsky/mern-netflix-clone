35f7487bebddc817dab75f647235ea55ada4e024
Phil Belinsky <philbelinsky@yahoo.com>
2025-05-13 20:32:00 -0400

    loading states added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const {login} = useAuthStore();
+    const {login, isLoggingIn} = useAuthStore();
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -59,8 +59,8 @@ const LoginPage = () => {
                         onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700'>
-                            Login
+                    <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700' disabled={isLoggingIn}>
+                            {isLoggingIn ? "Loading..." : "Login" }
                     </button>
                 </form>
                 <div className='text-center text-gray-400'>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthStore } from '../store/authUser'
+import { isObjectIdOrHexString } from 'mongoose'
 
 const SignUpPage = () => {
 
@@ -11,7 +12,7 @@ const SignUpPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const {signup} = useAuthStore();
+    const {signup, isSigningUp} = useAuthStore();
 
     const handleSignUp = (e) => {
         e.preventDefault();
@@ -75,8 +76,8 @@ const SignUpPage = () => {
                         onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700'>
-                            Sign Up
+                    <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700' disabled={isSigningUp}>
+                            {isSigningUp ? "Loading..." : "Sign Up"}
                     </button>
                 </form>
                 <div className='text-center text-gray-400'>
